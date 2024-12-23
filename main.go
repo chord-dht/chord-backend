11a3ba7834812b31d3e5b7b4055b234f87ed62ba
Xuanhao <shirou-emiya2002@outlook.com>
2024-12-23 20:05:57 +0100

    Fix: add cors

--- main.go
+++ main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"chord-backend/handlers"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST"},
+		AllowHeaders:     []string{"Content-Type"},
+		AllowCredentials: true,
+	}))
 
 	r.POST("/new", handlers.CreateNode)
 	r.GET("/initialize", handlers.InitializeNode)
