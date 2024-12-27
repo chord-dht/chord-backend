f5e487e365133176374d195bcb197e05fd575df6
Xuanhao <shirou-emiya2002@outlook.com>
2024-12-27 16:18:27 +0100

    api router

--- router/router.go
+++ router/router.go
@@ -1,18 +1,22 @@
 package router
 
 import (
+	"path"
+
 	"github.com/chord-dht/chord-backend/handlers"
 
 	"github.com/gin-gonic/gin"
 )
 
-func SetupRouter(r *gin.Engine) {
-	r.GET("/nodestatus", handlers.NodeStatus)
-	r.POST("/new", handlers.CreateNode)
-	r.GET("/initialize", handlers.InitializeNode)
-	r.GET("/quit", handlers.QuitNode)
-	r.GET("/printstate", handlers.GetNodeState)
-	r.POST("/storefile", handlers.StoreFile)
-	r.POST("/getfile", handlers.GetFile)
-	r.POST("/downloadfile", handlers.DownloadFile)
+func SetupAPIRouter(prefixPath string, r *gin.Engine) {
+	basePath := "/" + prefixPath
+
+	r.GET(path.Join(basePath, "nodestatus"), handlers.NodeStatus)
+	r.POST(path.Join(basePath, "new"), handlers.CreateNode)
+	r.GET(path.Join(basePath, "initialize"), handlers.InitializeNode)
+	r.GET(path.Join(basePath, "quit"), handlers.QuitNode)
+	r.GET(path.Join(basePath, "printstate"), handlers.GetNodeState)
+	r.POST(path.Join(basePath, "storefile"), handlers.StoreFile)
+	r.POST(path.Join(basePath, "getfile"), handlers.GetFile)
+	r.POST(path.Join(basePath, "downloadfile"), handlers.DownloadFile)
 }
